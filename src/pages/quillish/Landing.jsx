022bec6e082c795b280f3f70815588fa3b3b2a9d
Hyrum Sorensen <hyrum@lenderdock.com>
2023-08-23 16:57:15 -0600

    done for the day

--- src/pages/quillish/Landing.jsx
+++ src/pages/quillish/Landing.jsx
@@ -13,11 +13,13 @@ function landing() {
         <img alt="Quillish Logo" className="logo"src={logo1}/>
         <button className="createButton">Create account</button>
         <div className="d-flex flex-row justify-content-center iconsDiv">
-          <div>
+          <div className="d-flex flex-column align-items-center">
             <img alt="App Store" className="appLogo mx-lg-4 mx-2"src={appstorelogo}/>
+            <p>*Coming Soon</p>
           </div>
-          <div>
+          <div className="d-flex flex-column align-items-center">
             <img alt="Google Play" className="appLogo mx-lg-4 mx-2"src={googleplay}/>
+            <p>*Coming Soon</p>
           </div>
         </div>
       </div>
